e24272126705b420014c891fbbb8beba694251a4
Mohit Patel <mohitpatel2314@gmail.com>
2021-11-21 16:39:44 +0530

    fixes

--- src/components/designs/DashboardPanel.vue
+++ src/components/designs/DashboardPanel.vue
@@ -5,26 +5,29 @@
       <span class="card__tab"> All company cards </span>
     </div>
     <div class="dashboard__card-body row">
-      <div class="left-col col-xs-12 col-sm-6 col-md-6">
-          <!-- Carousel Section -->
+      <div class="left-col col-xs-10 col-sm-8 col-md-6">
+        <!-- Carousel Section -->
         <div class="carousel__section">
           <div class="view__card-number">
-            <img src="../../assets/remove_red_eye-24px.svg" alt="" style="margin-right:4px;"/> Show card
-            number
+            <img
+              src="../../assets/remove_red_eye-24px.svg"
+              alt=""
+              style="margin-right: 4px"
+            />
+            Show card number
           </div>
           <credit-card></credit-card>
         </div>
         <!-- Card features options -->
         <div class="features__section">
-
-<div v-for="feature in featuresList" :key="feature" class="feature">
-<div class="feature__icon">
-<img :src="require(`../../assets/${feature.icon}`)"  alt="">
-</div>
-<div class="feature__label">
-{{feature.label}}
-</div>
-</div>
+          <div v-for="feature in featuresList" :key="feature" class="feature">
+            <div class="feature__icon">
+              <img :src="require(`../../assets/${feature.icon}`)" alt="" />
+            </div>
+            <div class="feature__label">
+              {{ feature.label }}
+            </div>
+          </div>
         </div>
       </div>
       <div class="right-col col-xs-12 col-sm-8 col-md-6">
@@ -65,9 +68,8 @@
                 <div class="name">Hamleys</div>
                 <div class="date">20 May 2020</div>
                 <div class="refund__detail">
-                    <div class="icon">
-                    
-                  <img src="../../assets/business-and-finance.svg" alt="" />
+                  <div class="icon">
+                    <img src="../../assets/business-and-finance.svg" alt="" />
                   </div>
                   Refund on debit card
                 </div>
@@ -78,27 +80,38 @@
               </div>
             </div>
             <!--  -->
-             <div v-for="(transaction,k) in transactionsList" :key="k" class="transaction__row">
+            <div
+              v-for="(transaction, k) in transactionsList"
+              :key="k"
+              class="transaction__row"
+            >
               <div class="icon">
-                <img :src="require(`../../assets/${transaction.icon}`)" alt="" />
+                <img
+                  :src="require(`../../assets/${transaction.icon}`)"
+                  alt=""
+                />
               </div>
               <div class="info">
                 <div class="name">Hamleys</div>
                 <div class="date">20 May 2020</div>
                 <div class="refund__detail">
-                    <div class="icon">
-                    
-                  <img src="../../assets/business-and-finance.svg" alt="" />
+                  <div class="icon">
+                    <img src="../../assets/business-and-finance.svg" alt="" />
                   </div>
                   Refund on debit card
                 </div>
               </div>
               <div class="amount">
-                <span style="color:#000;">- S$ 150</span>&nbsp;
+                <span style="color: #000">- S$ 150</span>&nbsp;
                 <img src="../../assets/next.svg" alt="" />
               </div>
             </div>
+           
           </div>
+           <!-- View all transactions -->
+            <div class="view-all">
+                <span> View all transaction</span>
+            </div>
         </div>
       </div>
     </div>
@@ -111,46 +124,46 @@ export default {
   components: {
     CreditCard,
   },
-  data(){
-      return{
-          featuresList:[
-              {
-                  icon:"Freeze_card.svg",
-                  label:"Freeze card"
-              },
-              {
-                  icon:"spend_limit.svg",
-                  label:"Set spend limit"
-              },
-              {
-                  icon:"GPay.svg",
-                  label:"Add to GPay"
-              },
-              {
-                  icon:"Replace_card.svg",
-                  label:"Replace card"
-              },
-              {
-                  icon:"Deactivate_card.svg",
-                  label:"Cancel card"
-              },
-          ],
-          transactionsList:[
-              {
-                  icon:'flights.svg',
-                  name:'Hamleys'
-              },
-              {
-                  icon:'megaphone.svg',
-                  name:'Hamleys'
-              },
-              {
-                  icon:'file-storage.svg',
-                  name:'Hamleys'
-              },
-          ]
-      }
-  }
+  data() {
+    return {
+      featuresList: [
+        {
+          icon: "Freeze_card.svg",
+          label: "Freeze card",
+        },
+        {
+          icon: "spend_limit.svg",
+          label: "Set spend limit",
+        },
+        {
+          icon: "GPay.svg",
+          label: "Add to GPay",
+        },
+        {
+          icon: "Replace_card.svg",
+          label: "Replace card",
+        },
+        {
+          icon: "Deactivate_card.svg",
+          label: "Cancel card",
+        },
+      ],
+      transactionsList: [
+        {
+          icon: "flights.svg",
+          name: "Hamleys",
+        },
+        {
+          icon: "megaphone.svg",
+          name: "Hamleys",
+        },
+        {
+          icon: "file-storage.svg",
+          name: "Hamleys",
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -184,42 +197,40 @@ export default {
 .left-col {
   width: 55%;
 }
-.view__card-number{
-     width: 414px;
-     padding: 6px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    color: #01D167;
-    font-size: 12px;
-    font-weight: 600;
+.view__card-number {
+  width: 414px;
+  padding: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  color: #01d167;
+  font-size: 12px;
+  font-weight: 600;
 }
-.features__section{
-    padding: 16px;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    width:414px;
-    height: 116px;
-    background-color: #EDF3FF;
-    border-radius: 10px;
-    margin-top: 24px;
+.features__section {
+  padding: 16px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 414px;
+  height: 116px;
+  background-color: #edf3ff;
+  border-radius: 10px;
+  margin-top: 24px;
 
-    .feature{
-display: flex;
-flex-direction: column;
-align-items: center;
-        .feature__label{
-            color: #0C365A;
-            font-size: 13px;
-            margin-top: 6px;
-            text-align: center;
-            max-width: 60px;
-        }
+  .feature {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .feature__label {
+      color: #0c365a;
+      font-size: 13px;
+      margin-top: 6px;
+      text-align: center;
+      max-width: 60px;
     }
+  }
 }
-
-
 
 .right-col {
   width: 45%;
@@ -228,7 +239,6 @@ align-items: center;
 .accordian-icon {
   height: 54px;
   width: 24px;
- 
 }
 
 .accordian-header {
@@ -245,71 +255,82 @@ align-items: center;
   height: 72px;
   color: #0c365a;
   margin-bottom: 16px;
-.expanded-icon{
+  .expanded-icon {
     transform: rotate(180deg);
-}
+  }
   .--expanded {
     border: 1px solid #fcfcfc;
     height: 520px;
-    
   }
 }
 .accordian-header:hover {
   background-color: #f5f9ff;
 }
-.recent__transaction--content{
-    padding: 8px 16px;
-    outline: 1px solid #F0F0F0;
+.recent__transaction--content {
+  padding: 8px 16px;
+  outline: 1px solid #f0f0f0;
 }
-.transaction__row{
-    padding-bottom: 16px;
-    border-bottom: 1px solid #F0F0F0;
+.transaction__row {
+  padding-bottom: 16px;
+  border-bottom: 1px solid #f0f0f0;
+  display: flex;
+  margin: 14px 0px;
+  .icon {
+    height: 48px;
+    width: 48px;
+    background-color: #009dff1a;
+    border-radius: 50%;
     display: flex;
-margin: 14px 0px;
-    .icon{
-        height: 48px;
-        width: 48px;
-        background-color: #009DFF1A;
-        border-radius: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-.info{
+    align-items: center;
+    justify-content: center;
+  }
+  .info {
     margin-left: 12px;
-display: flex;
-flex-direction: column;
-width: 60%;
- .name{
-        font-size: 14px;
-        font-weight: 600;
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+    .name {
+      font-size: 14px;
+      font-weight: 600;
     }
-    .date{
-        font-size: 13px;
-      color: #AAAAAA;
+    .date {
+      font-size: 13px;
+      color: #aaaaaa;
       margin-top: 4px;
     }
-    .refund__detail{
-        margin-top: 8px;
-        font-size: 12px;
-        color: #325BAF;
+    .refund__detail {
+      margin-top: 8px;
+      font-size: 12px;
+      color: #325baf;
     }
-}
+  }
 
-.refund__detail{
-    display: flex   ;
-    .icon{
-        height: 20px;
-        width: 24px;
-        background-color: #325BAF;
-        margin-right: 8px;
+  .refund__detail {
+    display: flex;
+    .icon {
+      height: 20px;
+      width: 24px;
+      background-color: #325baf;
+      margin-right: 8px;
     }
-}
-.amount{
-    color: #01D167;
+  }
+  .amount {
+    color: #01d167;
     font-size: 14px;
     font-weight: 600;
+  }
 }
-   
+.view-all{
+    width:366px;
+    height: 42px;
+    background-color: #EDFFF5;
+    color: #01D167;
+    font-size: 13px;
+    font-weight: 600;
+    border: 1px solid #DDFFEC;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
 }
 </style>
